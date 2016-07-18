d2e0702d32598603f50e215cee108f966366cbcc
jmg2107 <jenjengoo@gmail.com>
2016-07-18 15:56:32 -0700

    add Gruntfile functionality for grunt test and grunt build corrects two lint errors, 1 on the server and 1 on the client filter initial request to yelp for top 5 results

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var assert = chai.assert;
+var should = chai.should();
+var expect = chai.expect;
+
+describe('Test the Client-Side', function() {
+  describe('Test the input boxes', function(){
+    it('should exist', function(){
+      expect(0).to.equal(0);
+    });
+
+  });
+});
