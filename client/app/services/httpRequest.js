d5822cdace810d5d3b7aed1306406ccd1a55c5d5
evenkeeler <connor.d.campbell@gmail.com>
2016-07-18 13:02:32 -0400

    add commented out factory for testing

---
+++ client/app/services/httpRequest.js
@@ -0,0 +1,35 @@
+// angular.module('sqrtl.httpRequest', [])
+//   .factory('Adventures', function($http){
+//     //requests venues that meet location and category criteria
+//     //TODO: add user parameters and such
+//     var requestAdventures = function(category, location){
+//       return $http({
+//         method: 'GET',
+//         url: '/api/getYelp',
+//         data: JSON.stringify({
+//           category: category,
+//           location: location
+//         })
+//       }).then(function(resp){
+//         return resp.data;
+//       });
+//     };
+
+//     return {
+//       requestAdventures: requestAdventures
+//     };
+
+//   });
+  // .factory('UserResponses', function($http){
+  //   //tells the database if a user accepted suggestions
+  //   var initialReaction = function(userName, restauranArray){
+  //     return $http({
+  //       method: 'POST',
+  //       url: '/adventure',
+  //       data: JSON
+  //     }).then(functon(){
+  //       //should do something.
+  //     })
+  //   }
+
+  // });
