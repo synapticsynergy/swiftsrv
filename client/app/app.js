43473617b13c91518e799bbd79a45cef71a05a9d
vlin59 <vlin59@gmail.com>
2016-07-19 14:56:44 -0500

    mend

--- client/app/app.js
+++ client/app/app.js
@@ -27,4 +27,25 @@ angular.module("sqrtl", [
         templateUrl: 'app/auth/auth.html',
         controller: 'AuthController'
       });
-  });
+
+  })
+  .factory('AttachTokens', function ($window) {
+    var attach = {
+      request: function (object) {
+        var jwt = $window.localStorage.getItem('com.sqrtl');
+        if (jwt) {
+          object.headers['x-access-token'] = jwt;
+        }
+        object.headers['Allow-Control-Allow-Origin'] = '*';
+        return object;
+      }
+    };
+    return attach;
+  })
+  // .run(function ($rootScope, $location, Auth) {
+  //   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+  //     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+  //       $location.path('/auth');
+  //     }
+  //   });
+  // });
