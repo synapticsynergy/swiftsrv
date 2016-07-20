0d190e7be25b064c4de5337aa28e929acbfd9a06
evenkeeler <connor.d.campbell@gmail.com>
2016-07-20 12:44:57 -0400

    add bug fix to scope changes in uber.js/html

--- client/app/uber/uber.js
+++ client/app/uber/uber.js
@@ -1,25 +1,27 @@
 angular.module("sqrtl.uber", [])
   .controller("UberController", function($scope, Adventures){
 
-    $scope.destination = { latitude: '0', longitude: '0'};
-    $scope.current = { latitude: '0', longitude: '0'};
+    var destination = { latitude: '0', longitude: '0'};
+    var current = { latitude: '0', longitude: '0'};
 
 
     $scope.geo = navigator.geolocation;
 
-    $scope.geoFindMe = function(){
+    $scope.geoFindMe = function(callback){
       $scope.geo.getCurrentPosition(function(success){
-        $scope.current.latitude = success.coords.latitude;
-        $scope.current.longitude = success.coords.longitude;
-        $scope.destination.latitude = window.localStorage.getItem('latitude');
-        $scope.destination.longitude = window.localStorage.getItem('longitude');
-        console.log($scope.current);
-        console.log($scope.destination);
+        callback(success);
       });
-
     };
+
 
    // $scope.getPrice()
 
-    $scope.geoFindMe();
+    $scope.geoFindMe(function(success){
+      $scope.$apply(function(){
+        $scope.current = {latitude: success.coords.latitude, longitude: success.coords.longitude};
+        $scope.destination = {latitude: window.localStorage.getItem('latitude'), longitude: window.localStorage.getItem('longitude')};
+        console.log($scope.current);
+        console.log($scope.destination);
+      });
+    });
   });
