420ee3b305e84f6c0ba6368586c3a36717c4c6f0
jmg2107 <jenjengoo@gmail.com>
2016-07-20 07:18:07 -0700

    Automatically load uber view Calculates user's coordinates and dest coords

--- client/app/uber/uber.js
+++ client/app/uber/uber.js
@@ -1,11 +1,22 @@
 angular.module("sqrtl.uber", [])
   .controller("UberController", function($scope, Adventures){
+
     $scope.destination = { latitude: '0', longitude: '0'};
+    $scope.current = { latitude: '0', longitude: '0'};
 
-    $scope.getDest = function(){
-      $scope.destination.latitude = sessionStorage.getItem('latitude');
-      $scope.destination.longitude = sessionStorage.getItem('longitude');
-    }
 
-    $scope.getDest();
+    $scope.geo = navigator.geolocation;
+
+    $scope.geoFindMe = function(){
+      $scope.geo.getCurrentPosition(function(success){
+        $scope.current.latitude = success.coords.latitude;
+        $scope.current.longitude = success.coords.longitude;
+        $scope.destination.latitude = sessionStorage.getItem('latitude');
+        $scope.destination.longitude = sessionStorage.getItem('longitude');
+        console.log($scope.current);
+      });
+
+    };
+
+    $scope.geoFindMe();
   });
