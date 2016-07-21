9b41887897e8c81ffb3918d449d366befde935ed
jmg2107 <jenjengoo@gmail.com>
2016-07-21 14:36:07 -0700

    add google maps angular lib shows/hides calculation of routes for uber

--- client/app/uber/uber.js
+++ client/app/uber/uber.js
@@ -1,11 +1,12 @@
-angular.module("sqrtl.uber", [])
-  .controller("UberController", function($scope, Adventures){
+angular.module("sqrtl.uber", ['ngLodash', 'uiGmapgoogle-maps'])
+  .controller("UberController", function($scope, lodash, Adventures){
 
     var destination = { latitude: '0', longitude: '0'};
     var current = { latitude: '0', longitude: '0'};
 
-
+    $scope.calculated = false;
     $scope.geo = navigator.geolocation;
+    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
 
     $scope.geoFindMe = function(callback){
       $scope.geo.getCurrentPosition(function(success){
@@ -45,6 +46,7 @@ angular.module("sqrtl.uber", [])
       $scope.destination = {latitude: window.localStorage.getItem('latitude'), longitude: window.localStorage.getItem('longitude')};
       console.log($scope.current);
       console.log($scope.destination);
+      $scope.calculated = true;
     });
   });
 });
