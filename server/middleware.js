51a5e475aedd0bfee40fb61137f667b026f50e1e
vlin59 <vlin59@gmail.com>
2016-07-21 13:12:08 -0500

    add stormpath to app

--- server/middleware.js
+++ server/middleware.js
@@ -1,10 +1,30 @@
 var bodyParser = require('body-parser');
-
-
+var stormpath = require('express-stormpath');
+var path = require('path');
 
 
 module.exports = function (app, express){
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(express.static('client'));
+
+
+  app.use(stormpath.init(app, {
+    web: {
+      spa: {
+        enabled: true,
+        view: path.join(__dirname, '..', 'client','index.html')
+      },
+      me: {
+        expand: {
+          customData: true,
+          groups: true
+        }
+      }
+    }
+  }));
+
+
+
+
 };
