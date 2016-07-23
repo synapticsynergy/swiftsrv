4328a624beb3eaa2bc16eae3c254ec8312106050
jmg2107 <jenjengoo@gmail.com>
2016-07-22 17:04:26 -0700

    add tests for Adventures factory changed versions for angular and angular-mocks to match for Karma testing

--- test/test.js
+++ test/test.js
@@ -1,13 +1,44 @@
-describe('Adventures', function () {
-  var $httpBackend;
+describe('Adventures Factory', function () {
+  var $httpBackend, Adventures;
   beforeEach(module('sqrtl.httpRequest'));
 
-  beforeEach(inject(function (_$httpBackend_, _lodash_) {
-    $httpBackend = _$httpBackend_;
+  afterEach(inject(function ($httpBackend) {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
   }));
 
-  it('exists', function () {
-    expect(0).to.be(0);
+  beforeEach(inject(function ($injector) {
+    $httpBackend = $injector.get('$httpBackend');
+    Adventures = $injector.get('Adventures');
+  }));
+
+  it('Adventures factory should exist', function () {
+    expect(Adventures).to.exist;
   });
+
+  it('Adventures has method requestAdventures', function () {
+    expect(Adventures.requestAdventures).to.be.a('function');
+  });
+
+  it('Adventures has method dataShift', function () {
+    expect(Adventures.dataShift).to.be.a('function');
+  });
+
+  it('Adventures has method getUber', function () {
+    expect(Adventures.getUber).to.be.a('function');
+  });
+
+  it('Adventures has method uberPrice', function () {
+    expect(Adventures.uberPrice).to.be.a('function');
+  });
+
+  it('Adventures has method uberRide', function () {
+    expect(Adventures.uberRide).to.be.a('function');
+  });
+
+  it('Adventures has method geoFindMe', function () {
+    expect(Adventures.geoFindMe).to.be.a('function');
+  });
+
 });
 
