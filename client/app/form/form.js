7bdf06a1431c75639a460e284071d1644c642d9f
Blake Danson <blakedanson@gmail.com>
2016-10-12 23:20:27 -0400

    (clean) Remove buggy geoloc script

--- client/app/form/form.js
+++ client/app/form/form.js
@@ -11,6 +11,7 @@ angular.module("sqrtl.form", ['ngTouch'])
     $scope.cllYelp = undefined;
     $scope.calculating = false;
     $scope.location = undefined;
+    $scope.fillCurrentLoc = undefined;
 
     $scope.getLocationAndCategory = function(location, category){
       Adventures.requestAdventures(location, category, $scope.cllYelp)
@@ -27,34 +28,40 @@ angular.module("sqrtl.form", ['ngTouch'])
         });
     };
 
-    // $scope.findMe = function(){
-    //   $scope.calculating = true;
-    //   Adventures.geoFindMe(function(success){
-    //     $scope.$apply(function(){
-    //        $scope.cll = {latitude: success.coords.latitude, longitude: success.coords.longitude};
-    //        $scope.cllYelp = success.coords.latitude + "," + success.coords.longitude;
-    //        LocationFactory.setCoordinates($scope.cll);
-    //        console.log("cll", $scope.cll);
-    //        $scope.reverseGeocode();
-    //        $scope.calculating = false;
-    //     });
-    //   });
-    // };
+    $scope.findMe = function(){
+      $scope.calculating = true;
+      Adventures.geoFindMe(function(success){
+        $scope.$apply(function(){
+           $scope.cll = {latitude: success.coords.latitude, longitude: success.coords.longitude};
+           $scope.cllYelp = success.coords.latitude + "," + success.coords.longitude;
+           LocationFactory.setCoordinates($scope.cll);
+           console.log("cll", $scope.cll);
+           $scope.reverseGeocode();
+           $scope.calculating = false;
+        });
+      });
+    };
 
-    // $scope.reverseGeocode = function(){
 
-    //   var latlng = new google.maps.LatLng($scope.cll.latitude, $scope.cll.longitude);
-    //   $scope.geocoder.geocode({'latLng': latlng}, function(results, status){
-    //     if (status == google.maps.GeocoderStatus.OK) {
-    //       $scope.$apply(function(){
-    //         console.log("geocode results ", results[0].formatted_address);
-    //         $scope.location = results[0].formatted_address;
-    //       });
-    //     } else {
-    //       console.log("Geocoder failed due to: " + status);
-    //     }
-    //   });
-    // };
+
+    $scope.reverseGeocode = function(){
+
+       var geocoder = new google.maps.Geocoder();
+       var latLng = new google.maps.LatLng($scope.cll.latitude, $scope.cll.longitude);
+
+       if (geocoder) {
+          geocoder.geocode({ 'latLng': latLng}, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+              $scope.$apply(function(){
+                console.log("geocode results ", results[0].formatted_address);
+                $scope.location = results[0].formatted_address;
+              });
+            } else {
+              console.log("Geocoder failed due to: " + status);
+            }
+          });
+        }
+    };
 
 
 
