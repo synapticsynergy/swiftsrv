d987f672a2e16c20fd937e13a87d6f16b737b0fe
Blake Danson <blakedanson@Blakes-MacBook-Pro.local>
2017-03-09 15:52:34 -0800

    (debug) Remove coord references, render data

--- client/app/adventureLand/Adventure.js
+++ client/app/adventureLand/Adventure.js
@@ -5,27 +5,29 @@ angular.module('sqrtl.adventure', ["ngTouch"])
   /**************************/
 
   //sets the scope data from local storage
-  console.log(window.localStorage.getItem('data'));
+  var dataShape = JSON.parse(window.localStorage.getItem('data'))[0];
+  console.log(dataShape);
   $scope.data = JSON.parse(window.localStorage.getItem('data'))[0];
 
   //gets distance to venue and formats it to kilometers,
   //it will not render to the page if the distance isnt availible
-  var distance = LocationFactory.findDistance($scope.data.location.coordinate);
-  if (distance) {
-    distanceCalc = Math.round(distance * 10 * 0.62137)/10 + ' Mi';
-    window.localStorage.setItem('distanceCalc', distanceCalc);
-    $scope.distance = window.localStorage.getItem('distanceCalc');
-  } else if (window.localStorage.getItem('distanceCalc')) {
-    $scope.distance = window.localStorage.getItem('distanceCalc');
-  } else {
-    $scope.distance = undefined;
-  }
+
+  // var distance = LocationFactory.findDistance($scope.data.location.coordinate);
+  // if (distance) {
+  //   distanceCalc = Math.round(distance * 10 * 0.62137)/10 + ' Mi';
+  //   window.localStorage.setItem('distanceCalc', distanceCalc);
+  //   $scope.distance = window.localStorage.getItem('distanceCalc');
+  // } else if (window.localStorage.getItem('distanceCalc')) {
+  //   $scope.distance = window.localStorage.getItem('distanceCalc');
+  // } else {
+  //   $scope.distance = undefined;
+  // }
 
   //assigns the coordinates and google maps url
   $scope.address = {
-    long: $scope.data.location.coordinate.longitude,
-    lat: $scope.data.location.coordinate.latitude,
-    // templateUrl: 'http://maps.google.com/maps?q=' + $scope.data.location.coordinate.latitude + ',' + $scope.data.location.coordinate.longitude
+    // long: $scope.data.location.coordinate.longitude,
+    // lat: $scope.data.location.coordinate.latitude,
+
     templateUrl: 'http://maps.google.com/maps?q=' + $scope.data.location.address + ',' + $scope.data.location.city
   };
 
@@ -36,17 +38,17 @@ angular.module('sqrtl.adventure', ["ngTouch"])
   $scope.getNew = function(){
     Adventures.dataShift();
     $scope.data = JSON.parse(window.localStorage.getItem('data'))[0];
-    var distance = LocationFactory.findDistance($scope.data.location.coordinate);
-    if (distance) {
-      distanceCalc = Math.round(distance * 10 * 0.62137)/10 + ' Mi';
-      window.localStorage.setItem('distanceCalc', distanceCalc);
-      $scope.distance = window.localStorage.getItem('distanceCalc');
-    } else if (window.localStorage.getItem('distanceCalc')) {
-      $scope.distance = window.localStorage.getItem('distanceCalc');
-    } else {
-      console.log('code was run...');
-      $scope.distance = undefined;
-    }
+    // var distance = LocationFactory.findDistance($scope.data.location.coordinate);
+    // if (distance) {
+    //   distanceCalc = Math.round(distance * 10 * 0.62137)/10 + ' Mi';
+    //   window.localStorage.setItem('distanceCalc', distanceCalc);
+    //   $scope.distance = window.localStorage.getItem('distanceCalc');
+    // } else if (window.localStorage.getItem('distanceCalc')) {
+    //   $scope.distance = window.localStorage.getItem('distanceCalc');
+    // } else {
+    //   console.log('code was run...');
+    //   $scope.distance = undefined;
+    // }
   };
 
   //requsts uber with you coordinates and then redirects your to ubers login
