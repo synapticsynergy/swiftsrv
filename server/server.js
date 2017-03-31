4198c8b36b8249e34ae092caacb06746ff181716
Blake Danson <blakedanson@Blakes-MacBook-Pro.local>
2017-03-31 13:42:18 -0700

    (feat) Test CORS for API

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 //var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
+
+app.use(cors());
 
 require('./middleware.js')(app, express);
 require('./routes.js')(app, express);
